dae4a91836e8d89e0ee4c85871299c01341518d4
Ignacio Imbrogno <ignacio.imbrogno@gmail.com>
2023-04-11 13:57:21 -0300

    Proyecto limpio, con la base y los archivos reacomodades listo para comenzar a usarse

---
+++ Tercer_e_imbrogno/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tercer_e_imbrogno project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tercer_e_imbrogno.settings')
+
+application = get_wsgi_application()
